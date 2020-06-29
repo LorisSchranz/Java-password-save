a12b0875ad013d51af33334a8450dd34fa77e76e
Schranz Loris, GHR-OSA-NEX-31 <Loris.Schranz@swisscom.com>
2020-06-29 20:47:22 +0200

    Delivery of project v2.0 (Way better than before, trust me, it's great)

--- PasswordManager/src/main/java/ch/bbw/lskf/PasswordManager/Controllers/MainController.java
+++ PasswordManager/src/main/java/ch/bbw/lskf/PasswordManager/Controllers/MainController.java
@@ -1,8 +1,9 @@
-   package ch.bbw.lskf.PasswordManager.Controllers;
+package ch.bbw.lskf.PasswordManager.Controllers;
 
 import ch.bbw.lskf.PasswordManager.Models.Password;
 import ch.bbw.lskf.PasswordManager.Models.User;
 import ch.bbw.lskf.PasswordManager.Services.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,8 @@ import java.util.ArrayList;
 
 @Controller
 public class MainController {
-private ArrayList<Password> passwords = new ArrayList<>();
+    @Autowired
+    private FileService fileService;
 //public PasswordController  passwordController = new PasswordController();
 
     // Login
@@ -28,41 +30,47 @@ private ArrayList<Password> passwords = new ArrayList<>();
     }
 
     @PostMapping("/login")
-    public String login(Model model, @ModelAttribute User user, Password password){
-        //TODO: check login
-        if (user.getUsername().equals("Admin")){
-            FileService.writeFile();
-            //TODO: json file with all passwords
-        }
-        model.addAttribute("passwords", passwords);
-        return "password";
+    public String login(@ModelAttribute User user) {
+        return "redirect:/password";
     }
 
-    @GetMapping("/login")
-    public String password(Model model, Password password) {
-        model.addAttribute("password", password);
+
+    @GetMapping("/password")
+    public String password(Model model) {
+        model.addAttribute("passwords", fileService.getPasswords());
         return "password";
     }
-
-    // Logout
 
     // Creating Entries
     @GetMapping("/newEntry")
     public String entry(Model model, Password password) {
         model.addAttribute("password", password);
-        //passwordController.writeInDB(password);
         return "newEntry";
     }
 
     @PostMapping("/createEntry")
-    public String newEntry(Model model, @ModelAttribute Password password) {
-        passwords.add(password);
-        model.addAttribute("passwords", passwords);
-        //passwordController.writeInDB(password);
-        return "password";
+    public String newEntry(@ModelAttribute Password password) {
+        if (password.getId() == 0) {
+            password.setId(fileService.getPasswords().size() + 1);
+        }
+        FileService.writeFile(password);
+        return "redirect:/password";
     }
 
     // Editing Entries
+    @GetMapping("/openEntry")
+    public String openEntry(Model model, @RequestParam int id) {
+        model.addAttribute("password", fileService.getPasswordByID(id));
+        fileService.removeByID(id);
+        FileService.rewriteFile();
+        return "newEntry";
+    }
+
 
     // Delete Entries
+    @GetMapping("/deleteEntry")
+    public String deleteEntry(@RequestParam int id) {
+        fileService.removeByID(id);
+        return "redirect:/password";
+    }
 }
